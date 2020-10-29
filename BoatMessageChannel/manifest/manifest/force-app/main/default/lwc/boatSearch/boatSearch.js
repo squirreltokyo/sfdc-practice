77e3ab253d4d17a63354deeca6af54836f075634
pleasetellme@163.com <pleasetellme@163.com>
2020-10-30 00:29:01 +0900

    Lightning Web Components Specialist step 9 after

--- BoatMessageChannel/manifest/manifest/force-app/main/default/lwc/boatSearch/boatSearch.js
+++ BoatMessageChannel/manifest/manifest/force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,17 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class BoatSearch extends LightningElement {
-    isLoading = false;
-    
+export default class BoatSearch extends NavigationMixin(LightningElement) {
+    @track isLoading = false;
+
     // Handles loading event
-    handleLoading() { }
-    
+    handleLoading() {
+        this.isLoading = true;
+    }
+
     // Handles done loading event
-    handleDoneLoading() { }
-    
+    handleDoneLoading() {
+        this.isLoading = false;
+    }
+
     // Handles search boat event
     // This custom event comes from the form
-    searchBoats(event) { }
-    
-    createNewBoat() { }
+    searchBoats(event) {
+        const boatTypeId = event.detail.boatTypeId;
+        this.template.querySelector("c-boat-search-results").searchBoats(boatTypeId);
+    }
+
+    createNewBoat() {
+        // Navigate to the New Boat page
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Boat__c',
+                actionName: 'new',
+            },
+        });
+    }
 }
