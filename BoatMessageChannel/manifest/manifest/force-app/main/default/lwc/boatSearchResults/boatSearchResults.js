06ecd7ba7df1f9493b842d5d6d640dc3e83a371b
pleasetellme@163.com <pleasetellme@163.com>
2020-10-29 22:50:16 +0900

    Lightning Web Components Specialist step 8 after

--- BoatMessageChannel/manifest/manifest/force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
+++ BoatMessageChannel/manifest/manifest/force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
@@ -2,17 +2,18 @@ import { LightningElement, wire, api, track } from 'lwc';
 import getBoats from '@salesforce/apex/BoatDataService.getBoats';
 import updateBoatList from '@salesforce/apex/BoatDataService.updateBoatList';
 import { getRecordNotifyChange } from 'lightning/uiRecordApi';
-//import { updateRecord } from 'lightning/uiRecordApi';
+import { updateRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { refreshApex } from '@salesforce/apex';
 import { publish, MessageContext } from 'lightning/messageService';
-import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+import BoatMC from '@salesforce/messageChannel/BoatMessageChannel__c';
 
 const SUCCESS_TITLE = 'Success';
 const MESSAGE_SHIP_IT = 'Ship it!';
 const SUCCESS_VARIANT = 'success';
-const CONST_ERROR = 'Error';
+const ERROR_TITLE = 'Error';
 const ERROR_VARIANT = 'error';
+const CONST_ERROR = 'Error';
 
 export default class BoatSearchResults extends LightningElement {
   selectedBoatId = '';
@@ -32,7 +33,7 @@ export default class BoatSearchResults extends LightningElement {
   // wired message context
   @wire(MessageContext) messageContext;
   // wired getBoats method
-  @wire(getBoats, {boatTypeId: '$boatTypeId'})
+  @wire(getBoats, { boatTypeId: '$boatTypeId' })
   wiredBoats(result) {
     this.boats = result;
     if (result.error) {
@@ -67,13 +68,12 @@ export default class BoatSearchResults extends LightningElement {
   updateSelectedTile(event) {
     this.selectedBoatId = event.detail.boatId;
     this.sendMessageService(this.selectedBoatId);
-    return this.refresh();
   }
   
   // Publishes the selected boat Id on the BoatMC.
   sendMessageService(boatId) {
     // explicitly pass boatId to the parameter recordId
-    publish(this.messageContext, BOATMC, { recordId : boatId });
+    publish(this.messageContext, BoatMC, { recordId : boatId });
   }
   
   // The handleSave method must save the changes in the Boat Editor
@@ -82,39 +82,36 @@ export default class BoatSearchResults extends LightningElement {
   // Show a toast message with the title
   // clear lightning-datatable draft values
   handleSave(event) {
-    // notify loading
     this.notifyLoading(true);
+    const recordInputs = event.detail.draftValues.slice().map(draft=>{
+       const fields = Object.assign({}, draft);
+       return {fields};
+   });
 
-    const updatedFields = event.detail.draftValues;
-    const notifyChangeIds = updatedFields.map(row => { return { "recordId": row.Id } });
-
-    // Update the records via Apex
-    updateBoatList({data: updatedFields})
-      .then(res => {
-      this.dispatchEvent(
-        new ShowToastEvent({
-            title: SUCCESS_TITLE,
-            message: MESSAGE_SHIP_IT,
-            variant: SUCCESS_VARIANT
-        })
-      );
-      
-      getRecordNotifyChange(notifyChangeIds);
-
-      this.draftValues = [];
-      return this.refresh();
-    }).catch(error => {
-      this.error = error;
-      this.dispatchEvent(
+   console.log(recordInputs);
+   const promises = recordInputs.map(recordInput => updateRecord(recordInput));
+   Promise.all(promises).then(res => {
+       this.dispatchEvent(
            new ShowToastEvent({
-               title: CONST_ERROR,
-               message: error.body.message,
-               variant: ERROR_VARIANT
+               title: SUCCESS_TITLE,
+               message: MESSAGE_SHIP_IT,
+               variant: SUCCESS_VARIANT
            })
        );
-       this.notifyLoading(false);
-    }).finally(() => {
-      this.draftValues = [];
+       this.draftValues = [];
+       return this.refresh();
+   }).catch(error => {
+       this.error = error;
+       this.dispatchEvent(
+            new ShowToastEvent({
+                title: ERROR_TITLE,
+                message: CONST_ERROR,
+                variant: ERROR_VARIANT
+            })
+        );
+        this.notifyLoading(false);
+   }).finally(() => {
+        this.draftValues = [];
     });
   }
   // Check the current value of isLoading before dispatching the doneloading or loading custom event
